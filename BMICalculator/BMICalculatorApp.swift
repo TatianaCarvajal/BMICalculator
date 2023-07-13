4d6b0f3dce3139ffd5e4f83552d31f81cd944117
Tatiana Carvajal <taticv159912@gmail.com>
2023-07-13 11:41:02 -0500

    first commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Tatiana Carvajal on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
